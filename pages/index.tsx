547552b9832af00b2090023ef5c8743f78d38ef5
mhdginola <mhdginola@gmail.com>
2022-05-25 16:37:40 +0700

    add bootstrap and form contact

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,12 @@
 import EyeN3 from '../masok/Component/EyeN3/EyeN3';
+import ContactUsForm from '../masok/Container/ContactUsForm/ContactUsForm';
 import Layout1 from '../masok/Layout/Layout1/Layout1';
 
 export default function Home(){
   return (
     <Layout1>
       <EyeN3/>
+      <ContactUsForm/>
     </Layout1>
   )
 }
