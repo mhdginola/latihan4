f2917f608afa908412867069e31bd1fd0ea91f6f
mhdginola <mhdginola@gmail.com>
2022-05-25 10:31:06 +0700

    memasukkan card product

---
+++ masok/Component/CardItem/CardItemV2/CardItemV2.tsx
@@ -0,0 +1,78 @@
+import st from './CardItemV2.module.css';
+// import dataProduct from '../../../Data/DataProduct/DataProduct';
+import Image from 'next/image';
+
+function CardItemV2(){
+    return(
+        <div className={st.bungkus}>
+            <div className={st.cardg} >
+                <div className={st.card_img}>                
+                    <div className={st.gbr}>
+                        <Image src="/images/1.png" width={500} height={500} alt='ITEM' priority/>
+                    </div>
+                </div>
+                <div className={st.card_header}>
+                    <h2>Product 1</h2>
+                    <p>Deskripsi</p>                                          
+                </div>
+            </div>
+            <div className={st.cardg} >
+                <div className={st.card_img}>                
+                    <div className={st.gbr}>
+                        <Image src="/images/1.png" width={500} height={500} alt='ITEM' priority/>
+                    </div>
+                </div>
+                <div className={st.card_header}>
+                    <h2>Product 1</h2>
+                    <p>Deskripsi</p>                                          
+                </div>
+            </div>
+            <div className={st.cardg} >
+                <div className={st.card_img}>                
+                    <div className={st.gbr}>
+                        <Image src="/images/1.png" width={500} height={500} alt='ITEM' priority/>
+                    </div>
+                </div>
+                <div className={st.card_header}>
+                    <h2>Product 1</h2>
+                    <p>Deskripsi</p>                                          
+                </div>
+            </div>
+            <div className={st.cardg} >
+                <div className={st.card_img}>                
+                    <div className={st.gbr}>
+                        <Image src="/images/1.png" width={500} height={500} alt='ITEM' priority/>
+                    </div>
+                </div>
+                <div className={st.card_header}>
+                    <h2>Product 1</h2>
+                    <p>Deskripsi</p>                                          
+                </div>
+            </div>
+            <div className={st.cardg} >
+                <div className={st.card_img}>                
+                    <div className={st.gbr}>
+                        <Image src="/images/1.png" width={500} height={500} alt='ITEM' priority/>
+                    </div>
+                </div>
+                <div className={st.card_header}>
+                    <h2>Product 1</h2>
+                    <p>Deskripsi</p>                                          
+                </div>
+            </div>
+            <div className={st.cardg} >
+                <div className={st.card_img}>                
+                    <div className={st.gbr}>
+                        <Image src="/images/1.png" width={500} height={500} alt='ITEM' priority/>
+                    </div>
+                </div>
+                <div className={st.card_header}>
+                    <h2>Product 1</h2>
+                    <p>Deskripsi</p>                                          
+                </div>
+            </div>
+        </div>
+        );    
+}
+
+export default CardItemV2;
--- pages/product.tsx
+++ pages/product.tsx
@@ -1,11 +1,12 @@
-import EyeN3 from '../masok/Component/EyeN3/EyeN3';
+// import EyeN3 from '../masok/Component/EyeN3/EyeN3';
 import Layout1 from '../masok/Layout/Layout1/Layout1';
-import ContactUsForm from '../masok/Container/ContactUsForm/ContactUsForm';
+// import ContactUsForm from '../masok/Container/ContactUsForm/ContactUsForm';
+import CardItemV2 from '../masok/Component/CardItem/CardItemV2/CardItemV2';
 
 export default function Product(){
   return (
     <Layout1>
-      <EyeN3/>
+      <CardItemV2/>
     </Layout1>
   )
 }
