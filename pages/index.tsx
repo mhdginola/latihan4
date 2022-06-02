869cd57572da26c65ea749a43d24855855b4a38f
mhdginola <mhdginola@gmail.com>
2022-06-02 16:11:52 +0700

    comment EyeN3

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import EyeN3 from '../masok/Component/EyeN3/EyeN3';
+// import EyeN3 from '../masok/Component/EyeN3/EyeN3';
 import ContactUsForm from '../masok/Container/ContactUsForm/ContactUsForm';
 import Layout1 from '../masok/Layout/Layout1/Layout1';
 
 export default function Home(){  
   return (
     <Layout1>      
-      <EyeN3/>
+      {/* <EyeN3/> */}
       <ContactUsForm/>            
     </Layout1>
   )
