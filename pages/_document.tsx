e5c4f3a664251cecc5a59ac6382cd99db9b82698
mhdginola <mhdginola@gmail.com>
2022-06-03 10:49:19 +0700

    update main page pakai slider

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,7 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>      
     </Html>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
-import EyeN3 from '../masok/Component/EyeN3/EyeN3';
+// import EyeN3 from '../masok/Component/EyeN3/EyeN3';
+import SlideContent from '../masok/Component/SlideContent/SlideContent';
 import ContactUsForm from '../masok/Container/ContactUsForm/ContactUsForm';
 import Layout1 from '../masok/Layout/Layout1/Layout1';
 
 export default function Home(){  
   return (
     <Layout1>      
-      <EyeN3/>
+      {/* <EyeN3/> */}
+      <SlideContent/>
       <ContactUsForm/>            
     </Layout1>
   )
