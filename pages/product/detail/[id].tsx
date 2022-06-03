94a89bee61f7b0879adf897f0a5e94101fff1e76
mhdginola <mhdginola@gmail.com>
2022-06-03 15:04:29 +0700

    tadi salah directory

--- pages/product/detail/[id].tsx
+++ pages/product/detail/[id].tsx
@@ -1,7 +1,7 @@
 import Layout1 from '../../../masok/Layout/Layout1/Layout1';
 import Detail from '../../../masok/Container/Detail/Detail';
 import {useRouter} from 'next/router';
-import FakeSkeleton from '../../../masok/Component/FakeSkeleton/FAkeSkeleton';
+import FakeSkeleton from '../../../masok/Component/FakeSkeleton/FakeSkeleton';
 
 export default function DetailL(){
     const routerLink = useRouter();
