66d769102bd15a140a7edc1590aac566a3ab073c
mhdginola <mhdginola@gmail.com>
2022-06-03 15:00:09 +0700

    test fake skeleton

---
+++ masok/Component/FakeSkeleton/FakeSkeleton.tsx
@@ -0,0 +1,8 @@
+import st from './FakeSkeleton.module.css';
+
+export default function FakeSkeleton(){
+    return(
+        <div className={st.fask}>                    
+        </div>
+    )
+}
--- pages/product/detail/[id].tsx
+++ pages/product/detail/[id].tsx
@@ -1,12 +1,14 @@
 import Layout1 from '../../../masok/Layout/Layout1/Layout1';
 import Detail from '../../../masok/Container/Detail/Detail';
 import {useRouter} from 'next/router';
+import FakeSkeleton from '../../../masok/Component/FakeSkeleton/FAkeSkeleton';
 
 export default function DetailL(){
     const routerLink = useRouter();
     const {id} = routerLink.query;
   return (
     <Layout1>
+      <FakeSkeleton/>
       <Detail
         id={id}        
       />
