b3aa6dcec7ae726bfe2e25b778f3d62dc64276b7
mhdginola <mhdginola@gmail.com>
2022-06-03 10:22:24 +0700

    merapikan spek

--- masok/Component/Spek/Spek.tsx
+++ masok/Component/Spek/Spek.tsx
@@ -1,9 +1,11 @@
+import st from './Spek.module.css';
+
 export default function Spek(props:any){
     return(
         <tr>
-            <td><h5>{props.sp} </h5></td>
-            <td><h5> : </h5></td>
-            <td><h5> {props.dt}</h5></td>
+            <td className={st.bdg}><h5>{props.sp} </h5></td>
+            <td className={st.bdg}><h5> : </h5></td>
+            <td className={st.bdg}><h5> {props.dt}</h5></td>
         </tr>
         // <h1>-- {props.sp} --,</h1>
         //     <h6>-- {props.dt} --,</h6>
