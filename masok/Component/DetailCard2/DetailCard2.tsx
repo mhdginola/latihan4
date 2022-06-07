6cb09858f9564811f1d9b707c1fedd1cbe00c2ed
mhdginola <mhdginola@gmail.com>
2022-06-07 11:04:35 +0700

    hilangkan caption detail card

--- masok/Component/DetailCard2/DetailCard2.tsx
+++ masok/Component/DetailCard2/DetailCard2.tsx
@@ -32,8 +32,8 @@ const DetailCard2 =(props:any)=>{
                                     <Image src={props.thumb} layout="fill" alt="back" objectFit="contain" quality={75} priority/>     
                                 </div>
                             <div className="carousel-caption d-none d-md-block">
-                                <h5>First slide label</h5>
-                                <p>Some representative placeholder content for the first slide.</p>
+                                {/* <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p> */}
                             </div>
                             </div>
                             <div className="carousel-item" data-bs-interval="5000">
@@ -42,8 +42,8 @@ const DetailCard2 =(props:any)=>{
                                     <Image src={props.thumb1} layout="fill" alt="back" objectFit="contain" quality={75} priority/>     
                                 </div>
                             <div className="carousel-caption d-none d-md-block">
-                                <h5>Second slide label</h5>
-                                <p>Some representative placeholder content for the second slide.</p>
+                                {/* <h5>Second slide label</h5>
+                                <p>Some representative placeholder content for the second slide.</p> */}
                             </div>
                             </div>
                             <div className="carousel-item" data-bs-interval="5000">
@@ -52,8 +52,8 @@ const DetailCard2 =(props:any)=>{
                                     <Image src={props.thumb2} layout="fill" alt="back" objectFit="contain" quality={75} priority/>     
                                 </div>
                             <div className="carousel-caption d-none d-md-block">
-                                <h5>Third slide label</h5>
-                                <p>Some representative placeholder content for the third slide.</p>
+                                {/* <h5>Third slide label</h5>
+                                <p>Some representative placeholder content for the third slide.</p> */}
                             </div>
                             </div>
                             <div className="carousel-item" data-bs-interval="5000">
@@ -62,8 +62,8 @@ const DetailCard2 =(props:any)=>{
                                     <Image src={props.thumb3} layout="fill" alt="back" objectFit="contain" quality={75} priority/>     
                                 </div>
                             <div className="carousel-caption d-none d-md-block">
-                                <h5>4 slide label</h5>
-                                <p>Some representative placeholder content for the 4 slide.</p>
+                                {/* <h5>4 slide label</h5>
+                                <p>Some representative placeholder content for the 4 slide.</p> */}
                             </div>
                             </div>
                         </div>
