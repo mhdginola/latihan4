de0799b6ca2d1d52bfd25a77d94ebd42be57a884
mhdginola <mhdginola@gmail.com>
2022-06-08 17:15:44 +0700

    update

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,10 +6,10 @@ import Script from 'next/script'
 function MyApp({ Component, pageProps }: AppProps) {
   return(
   <>
-  <Script strategy='lazyOnload' src={"https://www.googletagmanager.com/gtag/js?id=G-WC139SS7CS"}/>
+  {/* <Script strategy='lazyOnload' src={"https://www.googletagmanager.com/gtag/js?id=G-WC139SS7CS"}/>
   <Script strategy='lazyOnload'>
     {"window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-WC139SS7CS');"}
-  </Script>
+  </Script> */}
   <Script id='bootstrap-cdn' src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js'/>  
   <Component {...pageProps} />
   </>
