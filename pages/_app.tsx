03f33e4d12d87d9033fe220cca9c0490abf8ecd8
mhdginola <mhdginola@gmail.com>
2022-06-08 18:20:27 +0700

    add google analytic revisi

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,61 @@ import '../styles/globals.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import type { AppProps } from 'next/app'
 import Script from 'next/script'
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return(
-  <>
-  {/* <Script strategy='lazyOnload' src={"https://www.googletagmanager.com/gtag/js?id=G-WC139SS7CS"}/>
-  <Script strategy='lazyOnload'>
-    {"window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-WC139SS7CS');"}
-  </Script> */}
-  <Script id='bootstrap-cdn' src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js'/>  
-  <Component {...pageProps} />
-  </>
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: any) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Script id='bootstrap-cdn' src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js'/>  
+      <Component {...pageProps} />
+    </>
   )
 }
 
-export default MyApp
+export default App
+
+// function MyApp({ Component, pageProps }: AppProps) {
+//   return(
+//   <>
+
+//   <Script id='bootstrap-cdn' src='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js'/>  
+//   <Component {...pageProps} />
+//   </>
+//   )
+// }
+
+// export default MyApp
