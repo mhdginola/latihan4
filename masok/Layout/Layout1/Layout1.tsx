7ad736e91009a46cda86d85230d0c36c087b1127
mhdginola <mhdginola@gmail.com>
2022-06-08 11:45:48 +0700

    google search console

--- masok/Layout/Layout1/Layout1.tsx
+++ masok/Layout/Layout1/Layout1.tsx
@@ -15,7 +15,7 @@ export default function Layout1(props:LayoutProps) {
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
                 <meta name="theme-color" content="#000000" />
                 <meta name="description" content="Web site created using create-react-app"/>  
-                
+                <meta name="google-site-verification" content="6jWW6TQNWYBtpeObeyqARhGPSNsUFg0nmXvbdVK3qkc" />
                 <title>DEON | SMART LIVING</title>
                 <link rel="icon" type="image/png" href='/LD3.svg'/>
                 
